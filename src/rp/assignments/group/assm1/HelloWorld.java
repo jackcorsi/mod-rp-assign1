cd050ee69814de67aca8504f9ce421728516eae9
jxc1090 <jxc1090@student.bham.ac.uk>
2018-01-22 15:35:10 +0000

    Made it say hello

---
+++ src/rp/assignments/group/assm1/HelloWorld.java
@@ -0,0 +1,17 @@
+package rp.assignments.group.assm1;
+
+import lejos.nxt.Button;
+
+/**
+ * 
+ * @author jxc1090
+ * @title Hello World
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+		Button.waitForAnyPress();
+	}
+
+}
