969ffc0d4817dc1979ba217bb3606f701fd16e3d
ajb769@student.bham.ac.uk <ajb769@cca-lg36-1209.cs.bham.ac.uk>
2018-01-26 15:43:33 +0000

    Javadoc commenting

--- src/rp/assignments/group/assm1/HelloWorld.java
+++ src/rp/assignments/group/assm1/HelloWorld.java
@@ -3,12 +3,18 @@ package rp.assignments.group.assm1;
 import lejos.nxt.Button;
 
 /**
- * 
  * @author jxc1090
- * @title Hello World
+ * @author ajb769
+ * @author afp766
+ * 
+ * part 1 of Exercise 1
  */
 public class HelloWorld {
 	
+	/**
+	 * prints "Hello World" and waits for any button press
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 		Button.waitForAnyPress();
